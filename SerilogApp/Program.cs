adaa6d09718165a625e0ce2db58aaddf334f9c64
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-12-12 19:50:19 +0530

    Serilog Example for implementing logs

---
+++ SerilogApp/Program.cs
@@ -0,0 +1,44 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure serilog... #1 Variation
+//Log.Logger = new LoggerConfiguration()
+//    .MinimumLevel.Information()
+//    .WriteTo.File("logs/serilogapp-.txt", rollingInterval: RollingInterval.Day)
+//    .CreateLogger();
+
+
+// Configure serilog... #2 Variation
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
